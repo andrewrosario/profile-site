1750f5dabd847edea1a1dbfad4d663ade3fdbfd6
Andrew Rosario <rosario.andrew@gmail.com>
2019-11-20 14:38:10 -0600

    added linkedin and github images with links

--- src/about.js
+++ src/about.js
@@ -4,13 +4,17 @@ const About = () => {
     return ( 
         <div id='about'>
             <figure>
-                <img src='./profile-square.jpg' width='50%'></img>
+                <img id='profile-pic' src='./profile-square.jpg' width='50%'></img>
                 <figcaption><h1>Hi! I'm Andy.</h1></figcaption>
             </figure>
-            <div>
+            <div id='bio'>
                 I'm a software engineer, educator, and life-long learner passionate about solving hard problems creatively and collaboratively.
-                I have worked primarily in React and Ruby on Rails to complete projects, working with APIs and relational databases to structure information.
-                When I'm not working on software projects, my partner and I love to do DIY projects around the house, travel, and spend time with our family.
+                I have worked primarily in React and Ruby on Rails to complete projects, while developing APIs and relational databases to structure information.
+                When I'm not working on software projects, my partner and I love to do DIY projects around the house, travel, and spending time with our family.
+                <div id='links'>
+                    <a href='https://github.com/andrewrosario'><img src='./github.svg'></img></a>
+                    <a href='https://www.linkedin.com/in/andrew-rosario-20982333/'><img src='./linkedin.png'></img></a>
+                </div>
             </div>  
         </div>
      );
