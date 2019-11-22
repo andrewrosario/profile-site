a9df69c3329e6b35c849f00f4979af816f965cf8
Andrew Rosario <rosario.andrew@gmail.com>
2019-11-22 12:41:27 -0600

    moved project screenshots down

--- src/projectCard.js
+++ src/projectCard.js
@@ -5,14 +5,15 @@ const ProjectCard = (props) => {
     const {name, image, description, technologies, deployLink, gitHub } = props.project
     return ( 
         <Card className='m-2'>
-            <a href={deployLink}><Card.Img variant="top" src={image} alt={`Homepage for ${name}`}/></a>
+            
             <Card.Body>
                 <Card.Title><a href={deployLink}>{name}</a></Card.Title>
-                <Card.Text>
-                    {description}<br></br><br></br>
-                    Technologies: {technologies}<br></br><br></br>
-                    <a href={gitHub[0]}><img src='./github.svg' width='8%'></img></a>
+                <Card.Text className='mb-0'>
+                    <p>{description}<br></br><br></br>
+                    Technologies: {technologies}<br></br><br></br></p>
                 </Card.Text>
+                <a href={deployLink}><Card.Img className='mb-3' variant="top" src={image} alt={`Homepage for ${name}`}/></a>
+                <a href={gitHub[0]}><img src='./github.svg' width='8%'></img></a>
             </Card.Body>
         </Card>
      );
